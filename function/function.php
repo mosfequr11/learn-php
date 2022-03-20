fe075e1f277132538f4ee2233ace82fffb795626
mosfequr11 <mosfequr11@gmail.com>
2022-03-21 01:02:56 +0600

    change file name

---
+++ function/function.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Determine the argument is even or odd
+ */
+    function evenOrOdd($n){ //here, $n paramater
+        if($n%2==0){
+            return true;
+        }
+        return false;
+        
+        // else{
+        //     return false;
+        // }
+    }
+
+/**
+ * Must write the meaningful function name
+ */
+function isEven($n){ //here, $n paramater
+    if($n%2==0){
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Must write the meaningful function name
+ */
+
+function isOdd($n){ //here, $n paramater
+    if($n%2==0){
+        return false;
+    }
+    return true;
+}
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+    require_once "function/function.php";
+/**
+ * Determine the argument is even or odd
+ */
+    // $x=13;
+    $x=14;
+
+    if(evenOrOdd($x)){  //here, $x argument
+        echo "{$x} is an even number";
+    }else
+        echo "{$x} is an Odd number";
+
+
+    echo "<hr>";
+
+/**
+ * Must write the meaningful function name
+ * Using isEven Function
+ */
+    $y=24;
+    if(isEven($y)){  //here, $y argument
+        echo "{$y} is an even number";
+    }else
+        echo "{$y} is an Odd number";
+
+
+    echo "<hr>";
+
+/**
+ * Must write the meaningful function name
+ * Using isOdd Function
+ */
+    $z=34;
+    if(isOdd($z)){  //here, $z argument
+
+        // echo "<pre>";
+        // print_r($z);
+        // echo "</pre>";
+
+        echo "{$z} is an Odd number";
+    }else
+        // echo "<br>";
+        // echo "<pre>";
+        // print_r($z);
+        // echo "</pre>";
+        // echo "<br>";
+
+        echo "{$z} is an Even number";
