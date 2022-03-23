bbb1ec70a09a1829aa79d45d3d9422970338fff4
mosfequr11 <mosfequr11@gmail.com>
2022-03-23 22:19:32 +0600

    factorial

--- function/function.php
+++ function/function.php
@@ -1,23 +1,28 @@
 <?php
+
 /**
  * Determine the argument is even or odd
  */
-    function evenOrOdd($n){ //here, $n paramater
-        if($n%2==0){
-            return true;
-        }
-        return false;
-        
-        // else{
-        //     return false;
-        // }
+function evenOrOdd($n)
+{ //here, $n paramater
+    if ($n % 2 == 0) {
+        return true;
     }
+    return false;
+
+    // else{
+    //     return false;
+    // }
+}
 
 /**
  * Must write the meaningful function name
+ * isEven()
  */
-function isEven($n){ //here, $n paramater
-    if($n%2==0){
+
+function isEven($n)
+{ //here, $n paramater
+    if ($n % 2 == 0) {
         return true;
     }
     return false;
@@ -27,9 +32,30 @@ function isEven($n){ //here, $n paramater
  * Must write the meaningful function name
  */
 
-function isOdd($n){ //here, $n paramater
-    if($n%2==0){
+function isOdd($n)
+{ //here, $n paramater
+    if ($n % 2 == 0) {
         return false;
     }
     return true;
+}
+
+/**
+ * Must write the meaningful function name
+ * isFactorial()
+ */
+
+function isFactorial(int $a)
+{
+    $result = 1;
+    for ($i = $a; $i > 1; $i--) {
+        $result *= $i;
+        // echo "<pre>";
+        // print_r($result);
+        // echo "</pre>";
+        // var_dump($result);
+        // echo "</br>";
+    }
+    echo __FILE__ . ' : ' . __LINE__;
+    return $result;
 }
